dd238681f7f3bdaa5cae5fcf4bd3c24639e8420d
nexomega <nexomega.minecraft@gmail.com>
2026-02-20 22:29:42 +0100

    Added little stats system

--- src/utils/StateManager.ts
+++ src/utils/StateManager.ts
@@ -1,7 +1,30 @@
 import Player from "../characters/Player";
 import InteractionEntity from "../entities/InteractionEntity";
 
+export class Stats {
+    private morale : number = 50;
+    private ethique : number = 50;
 
+    public getMorale() : number {
+        return this.morale;
+    }
+
+    public addMorale(amount: number) {
+        this.morale = this.clamp(this.morale+amount, 0, 100);
+    }
+
+    public getEthique() : number {
+        return this.ethique;
+    }
+
+    public addEthique(amount: number) {
+        this.morale = this.clamp(this.morale + amount, 0, 100);
+    }
+
+    clamp(number: number, lower: number, upper: number) {
+        return Math.min(Math.max(number, lower), upper);
+    }
+}
 export enum State {
     PLAYING, CINEMATIC, LOADING
 }
@@ -10,5 +33,7 @@ export class StateManager {
     public static actualPlayer: Player;
     public static currectInteractionEntity: InteractionEntity;
     public static state: State = State.LOADING;
+    public static stats: Stats = new Stats();
 
 }
+
