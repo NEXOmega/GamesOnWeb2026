6caec41ef91602e2482a671c30ccaa3ec2d447fc
nexomega <nexomega.minecraft@gmail.com>
2026-03-24 13:47:58 +0100

    Really Simple Save system with b64 encoding and easy loading using factory in scene manager

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import BaseScene from './scenes/BaseScene'
 import MyScene from './scenes/BasicScene';
 import SceneManager from './scenes/SceneManager';
+import SaveManager from './utils/SaveManager';
 
 let game: BaseScene;
 
@@ -8,12 +9,20 @@ window.addEventListener('DOMContentLoaded', () => {
     
     SceneManager.init('renderCanvas');
 
-    SceneManager.changeScene("initial_scene", async () => {
+    SceneManager.registerScene("BunkerScene", async () => {
         const firstScene = new MyScene(SceneManager.engine, 'renderCanvas');
         await firstScene.initScene();
         
         return firstScene;
     });
+
+    if(SaveManager.hasSave()) {
+        SceneManager.loadFromSave(SaveManager.load());
+        console.log("Save chargé depuis le local storage")
+        return;
+    }
+    console.log("Aucune sauvegarde trouvée")
+    SceneManager.changeScene("BunkerScene")
 
 });
 window.addEventListener('resize', () => {
--- src/scenes/SceneManager.ts
+++ src/scenes/SceneManager.ts
@@ -1,10 +1,17 @@
-import { Engine, Scene } from "@babylonjs/core";
-import BaseScene from "./BaseScene"; // Ajuste le chemin
+import { Engine, Scene, Vector3 } from "@babylonjs/core";
+import BaseScene from "./BaseScene";
+import { GameSaveData } from "../utils/SaveManager";
+import { StateManager } from "../utils/StateManager";
+
+export type SceneFactory = () => Promise<BaseScene>;
 
 export default class SceneManager {
+    
     public static engine: Engine;
     public static currentScene: BaseScene | null = null;
     public static canvas: HTMLCanvasElement;
+
+    private static sceneRegistry: Map<string, SceneFactory> = new Map();
 
     private static sceneCache: Map<string, BaseScene> = new Map();
     private static currentSceneId: string | null = null;
@@ -24,6 +31,14 @@ export default class SceneManager {
         });
     }
 
+    public static registerScene(sceneId: string, factory: SceneFactory) {
+        if (this.sceneRegistry.has(sceneId)) {
+            console.warn(`La scène ${sceneId} est déjà enregistrée. Elle va être remplacée.`);
+        }
+        this.sceneRegistry.set(sceneId, factory);
+        console.log(`Scène ${sceneId} ajoutée au registre !`);
+    }
+
     private static setFade(opacity: number) {
         const overlay = document.getElementById("fadeOverlay");
         if (overlay) overlay.style.opacity = opacity.toString();
@@ -34,50 +49,63 @@ export default class SceneManager {
     }
 
     public static async changeScene(
-        targetSceneId: string, 
-        sceneFactory?: () => Promise<BaseScene>, 
-        keepCurrentInRAM: boolean = false
-    ) {
-        this.setFade(1);
-        await this.wait(500);
+            targetSceneId: string, 
+            keepCurrentInRAM: boolean = false
+        ) {
+            this.setFade(1);
+            await this.wait(500);
 
-        this.engine.stopRenderLoop();
+            this.engine.stopRenderLoop();
 
-        if (this.currentScene && this.currentSceneId) {
-            this.currentScene.detachControl();
+            if (this.currentScene && this.currentSceneId) {
+                this.currentScene.detachControl();
 
-            if (!keepCurrentInRAM) {
-                this.currentScene.dispose();
-                this.sceneCache.delete(this.currentSceneId);
+                if (!keepCurrentInRAM) {
+                    this.currentScene.dispose();
+                    this.sceneCache.delete(this.currentSceneId);
+                } else {
+                    this.sceneCache.set(this.currentSceneId, this.currentScene);
+                }
+            }
+
+            let nextScene = this.sceneCache.get(targetSceneId);
+
+            if (!nextScene) {
+                const factory = this.sceneRegistry.get(targetSceneId);
+
+                if (!factory) {
+                    throw new Error(`Impossible de charger "${targetSceneId}". La scène n'est ni en cache, ni dans le registre.`);
+                }
+
+                nextScene = await factory();
+                await nextScene.whenReadyAsync();
+                
+                this.sceneCache.set(targetSceneId, nextScene);
             } else {
-                this.sceneCache.set(this.currentSceneId, this.currentScene);
+                console.log(`Scène ${targetSceneId} chargée instantanément depuis la RAM !`);
             }
-        }
 
-        let nextScene = this.sceneCache.get(targetSceneId);
-
-        if (!nextScene) {
-            if (!sceneFactory) {
-                throw new Error(`La scène ${targetSceneId} n'est pas en cache et aucune fonction de création n'a été fournie.`);
-            }
-            nextScene = await sceneFactory();
-            await nextScene.whenReadyAsync();
+            this.currentScene = nextScene;
+            this.currentSceneId = targetSceneId;
             
-            this.sceneCache.set(targetSceneId, nextScene);
-        } else {
-            console.log(`Scène ${targetSceneId} chargée depuis la RAM !`);
+            this.currentScene.attachControl(true);
+
+            this.engine.runRenderLoop(() => {
+                this.currentScene?.render();
+            });
+
+            this.setFade(0);
+    }
+
+    public static loadFromSave(gameSaveData: GameSaveData) {
+        if(!this.sceneRegistry.has(gameSaveData.sceneId)) {
+            throw Error("Scene not in Scene Registry, can't load it.")
         }
 
-        this.currentScene = nextScene;
-        this.currentSceneId = targetSceneId;
-        
-        this.currentScene.attachControl();
-
-        this.engine.runRenderLoop(() => {
-            this.currentScene?.render();
+        this.changeScene(gameSaveData.sceneId).then((scene) => {
+            const playerPosition = new Vector3(gameSaveData.playerPosition.x, gameSaveData.playerPosition.y, gameSaveData.playerPosition.z);
+            StateManager.actualPlayer.setPosition(playerPosition);
         });
-
-        this.setFade(0);
     }
 
     public static clearCache() {
