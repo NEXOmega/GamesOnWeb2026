e931cecd8d449a394cbfdaf4c52e00b212ab9c97
nexomega <nexomega.minecraft@gmail.com>
2026-03-24 13:05:17 +0100

    Simple Scene Manager with cache to prevent some scene to be loaded again and again

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
 import BaseScene from './scenes/BaseScene'
 import MyScene from './scenes/BasicScene';
+import SceneManager from './scenes/SceneManager';
 
 let game: BaseScene;
 
 window.addEventListener('DOMContentLoaded', () => {
-    // Create the game using the 'renderCanvas'.
-    game = new MyScene('renderCanvas');
-  });
+    
+    SceneManager.init('renderCanvas');
 
+    SceneManager.changeScene("initial_scene", async () => {
+        const firstScene = new MyScene(SceneManager.engine, 'renderCanvas');
+        await firstScene.initScene();
+        
+        return firstScene;
+    });
+
+});
 window.addEventListener('resize', () => {
-    game.getEngine().resize();
+    SceneManager.engine.resize();
 });
