7a39c79819868516a4d42f65545a719d14b5fb10
nexomega <nexomega.minecraft@gmail.com>
2026-05-04 12:21:55 +0200

    Forgot to add Bunger

--- src/scenes/BunkerScene.ts
+++ src/scenes/BunkerScene.ts
@@ -21,53 +21,12 @@ import Pickable from '../entities/Pickable';
 import { ConsoleLogAction, TeleportAction } from '../actions/Action';
 import { instanceToPlain, plainToInstance } from 'class-transformer';
 import { SkyMaterial } from '@babylonjs/materials';
-import { loadEnvironmentFromConfig } from './SceneUtils';
+import { loadConfig } from './SceneUtils';
 
 export default class BunkerScene extends BaseScene {
 
-    async createScene() : Promise<void> {
-        this.cinematicCamera = new CinematicCamera(this, this.canvas);
-        const havokInstance = await HavokPhysics();
-        const havokPlugin = new HavokPlugin(true, havokInstance);
-
-        this.collisionsEnabled = true;
-        this.enablePhysics(new Vector3(0, -100, 0), havokPlugin);
-        // Hide/show the Inspector with Alt+I   
-        window.addEventListener("keydown", (ev) => {
-            if (ev.altKey && ev.key === 'i') {
-                if (this.debugLayer.isVisible()) {
-                    this.debugLayer.hide();
-                } else {
-                    this.debugLayer.show({ embedMode: true });
-                }
-            }
-        });
-
-        window.addEventListener("keydown", (ev) => {
-            if(ev.altKey && ev.key === 'c') {
-                if(this.activeCamera == this.cinematicCamera) {
-                    this.activeCamera = this.actualPlayer.playerCamera;
-                    StateManager.state = State.PLAYING;
-                } else {
-                    this.activeCamera = this.cinematicCamera;
-                    StateManager.state = State.CINEMATIC;
-                        this.cinematicCamera.moveTo(this.cinematicCamera.position, this.cinematicCamera.position.add(new Vector3(0,5,0)), this.cinematicCamera.rotation, new Vector3(0,0,0), 5)
-                }
-            }
-        })
-    }
-
     async createEnvironment(): Promise<void> {
-        await loadEnvironmentFromConfig("./models/bunker.json", this);
-
-
-        Player.CreateAsync(this, new Vector3(0, 10, 0)).then((player) => {
-            this.actualPlayer = player;
-            this.entityManager.addEntity(player);
-            this.activeCamera = player.playerCamera;
-
-            StateManager.state = State.PLAYING;
-        });
+        await loadConfig("./models/bunker.json", this);
 
     }
 }
